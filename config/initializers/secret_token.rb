891e4b7db891f10911e667d3b58f7c8e984516bc
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-06-07 14:21:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swotbot2::Application.config.secret_key_base = '3fb9ebac284c40acd9f6150f0cdc19aa4d8f7e6f45ded174473d31b36468aac2f8ce4c279ac189ed83557b41aa00ebefece6c07c38210ab9cec398c2321eab06'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swotbot2::Application.config.session_store :cookie_store, key: '_swotbot-2_session'
